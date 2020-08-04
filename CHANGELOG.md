51ac0336ccb56851e9df5fffe72e4330869451b7
Simon Rothert <puresamari@gmail.com>
2020-08-04 21:08:04 +0200

    More dynamic module path reading

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 0.0.5 - 4. August 2020
+### Fixed
+- More dynamic module path reading
+
 ## 0.0.4 - 31. July 2020
 ### Fixed
 - return value of collected module that aren't node_modules
--- src/moduleResolution.ts
+++ src/moduleResolution.ts
@@ -1,33 +1,45 @@
+import fs from 'fs';
 import path from 'path';
 
+function resolveFileName(file: string) {
+  try { return require.resolve(file); }
+  catch {
+    if (fs.existsSync(file + '.ts')) { return file + '.ts'; }
+    return file;
+  }
+}
 
 function resolveModulee(moduleName: string, base: string) {
-  var findNodeModules = require('find-node-modules') as (any: string) => string[];
-  const modules = findNodeModules(base);
-  const paths = modules.map(v => {
-    try {
-      return { nodeModule: true, file: require.resolve(
-        path.resolve(base, v, moduleName.replace(base + '/', ''))
-      ) };
-    } catch {
-      return null;
+  if (!path.extname(moduleName) || path.extname(moduleName) === '') {
+    var findNodeModules = require('find-node-modules') as (any: string) => string[];
+    const modules = findNodeModules(base);
+    const paths = modules.filter(v => path.extname(v) === '').map(v => {
+      try {
+        const absolutePath = path.resolve(base, v, moduleName.replace(base + '/', ''));
+        const relativePath = path.relative(__dirname, absolutePath);
+        const resolvedAbsolute = path.resolve(__dirname, relativePath);
+        if (resolvedAbsolute.indexOf('node_modules') >= 0) {
+          const moduleConfig = JSON.parse(fs.readFileSync(path.resolve(resolvedAbsolute, 'package.json'), 'utf-8')) as { main: string }
+          return { nodeModule: true, file: path.resolve(resolvedAbsolute, moduleConfig.main) };
+          // return { nodeModule: true, file: require.resolve('relativePath') };
+        }
+      } catch (e) {
+        return null;
+      }
+    }).filter(v => !!v);
+    if (paths && paths.length > 0) {
+      return paths![0] as { nodeModule: boolean, file: string };
     }
-  }).filter(v => !!v);
-  if (paths && paths.length > 0) {
-    return paths![0] as { nodeModule: boolean, file: string };
   }
 
-  try {
-    return { nodeModule: false, file: require.resolve(moduleName) };
-  } catch (e) {
-    try {
-      return { nodeModule: false, file: require.resolve(moduleName.replace(base + '/', '')) };
-    } catch (e) {
-      return { nodeModule: true, file: moduleName.replace(base + '/', '') };
-    }
+  return {
+    nodeModule: false,
+    file: resolveFileName(moduleName)
   }
 }
 
 export function resolveModule(moduleName: string, base: string) {
+  console.log('THIS GOES IN', moduleName, base);
+  console.log('THIS Comes out', resolveModulee(moduleName, base));
   return resolveModulee(moduleName, base);
 }
